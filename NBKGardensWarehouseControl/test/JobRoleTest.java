7bfd639c472745f126d9df81452c80400b18fb10
Lachlan Bennett <goodfreddo@yahoo.co.uk>
2016-09-19 18:08:04 +0000

    Added Job role constructor test

--- NBKGardensWarehouseControl/test/JobRoleTest.java
+++ NBKGardensWarehouseControl/test/JobRoleTest.java
@@ -9,18 +9,22 @@ public class JobRoleTest
 		boolean checkPick = false;
 		boolean checkPack = true;
 		COEventType pick = new COEventType("pick");
-		COEventType pack = new COEventType(pick,"pack");
+		COEventType pack = new COEventType(pick, "pack");
 		jobRole.getEventsAuthorised().add(pick);
-		checkPick=jobRole.CheckAuthorisation(pick);
-		checkPack=jobRole.CheckAuthorisation(pack);
+		checkPick = jobRole.CheckAuthorisation(pick);
+		checkPack = jobRole.CheckAuthorisation(pack);
 		assertTrue("Job pick can be done", checkPick);
 		assertFalse("Job pack can be done", checkPack);
 	}
 	@Test
 	public void JobRole()
 	{
-		// jobRole.getEventsAuthorised() != null && jobRole.getJobName() ==
-		// "grunt"
-		throw new RuntimeException("Test not implemented");
+		JobRole jobRole = new JobRole();
+		boolean jobRoleExists = false;
+		if (jobRole.getEventsAuthorised() != null && jobRole.getJobName() == "grunt")
+		{
+			jobRoleExists = true;
+		}
+		assertTrue("Job Role exists", jobRoleExists);
 	}
 }
